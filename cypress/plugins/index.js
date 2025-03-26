49e51b08ed272759191425e0f4a9a74bf5f69031
Adambisk <adambisk209@gmail.com>
2025-03-26 10:53:25 +0530

    Delete cypress directory

--- cypress/plugins/index.js
+++
@@ -1,6 +0,0 @@
-// cypress/plugins/index.js
-module.exports = (on, config) => {
-  // This is where you would register any Cypress plugins
-  require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin(on, config);
-  return config;
-};
